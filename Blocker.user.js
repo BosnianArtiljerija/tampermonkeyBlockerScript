15ecaad3ea5fd5c915b57292135b9fb876f5bbc5
Artiljerija <okinamatara@mai.ru>
2026-03-28 14:32:02 +0100

    add gemini

--- Blocker.user.js
+++ Blocker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Blocker
 // @namespace    http://tampermonkey.net/
-// @version      2026-03-35
+// @version      2026-03-36
 // @description  try to take over the world!
 // @author       You
 // @downloadURL  https://raw.githubusercontent.com/BosnianArtiljerija/tampermonkeyBlockerScript/main/Blocker.user.js
