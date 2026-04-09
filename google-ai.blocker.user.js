81b0f2f34d894ddfa1f6d539ba38e6fe1865b4b2
Artiljerija <okinamatara@mai.ru>
2026-04-09 23:04:28 +0200

    nuke ai

--- google-ai.blocker.user.js
+++ google-ai.blocker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         google-ai-blocker
 // @namespace    http://tampermonkey.net/
-// @version      5
+// @version      6
 // @description  try to take over the world!
 // @author       You
 // @downloadURL  https://raw.githubusercontent.com/BosnianArtiljerija/tampermonkeyBlockerScript/main/google-ai.blocker.user.js
@@ -18,6 +18,10 @@
     function block1() {
         [...document.querySelectorAll("div[role=listitem]")]
         .filter(x => x.innerText==="Режим ИИ" || x.innerText==="KI‑Modus")
+        .forEach(x => x.hidden=true)
+
+        [...document.querySelectorAll("textarea")]
+        .filter(x => x.innerText==="Задайте вопрос" || x.innerText==="Frage senden")
         .forEach(x => x.hidden=true)
     }
 
