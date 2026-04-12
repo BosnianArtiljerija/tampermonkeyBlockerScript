05c30f4d6cb912fe48285578d57dba6667650c7e
Artiljerija <okinamatara@mai.ru>
2026-04-12 14:27:47 +0200

    desktop

--- Blocker.user.js
+++ Blocker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Blocker
 // @namespace    http://tampermonkey.net/
-// @version      2026-03-48
+// @version      2026-03-49
 // @description  try to take over the world!
 // @author       You
 // @downloadURL  https://raw.githubusercontent.com/BosnianArtiljerija/tampermonkeyBlockerScript/main/Blocker.user.js
