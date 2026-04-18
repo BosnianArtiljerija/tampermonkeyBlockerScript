47246ac9ea6e7bb877968236e598b202c1ca1de9
Artiljerija <okinamatara@mai.ru>
2026-04-18 21:29:56 +0200

    replace chatgpt with something else

---
+++ chatgpt-ai.user.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         chatgpt-stopwords
+// @namespace    http://tampermonkey.net/
+// @version      1
+// @description  try to take over the world!
+// @author       You
+// @downloadURL  https://raw.githubusercontent.com/BosnianArtiljerija/tampermonkeyBlockerScript/main/chatgpt-ai.user.js
+// @match *://*.chatgpt.com/*
+// @match *://chatgpt.com/*
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const condition = true;
+
+    if (condition) {
+        function block1() {
+            if (document.documentElement) {
+                document.documentElement.innerHTML = "Nö";
+            }
+            if (document.head) {
+                document.head.innerHTML = "";
+            }
+            if (document.body) {
+                document.body.innerHTML="Nö";
+            }
+        }
+
+
+        try {
+            block1();
+        } catch (e) {}
+
+        setInterval(block1, 100);
+    }
+
+})();
